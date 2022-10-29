35064679a10c3d58b114b6a029189ef1409356ff
PankajWorkspace <116002604+PankajWorkspace@users.noreply.github.com>
2022-10-29 17:14:21 +0530

    Add files via upload

---
+++ Project Tab/Tab.js
@@ -0,0 +1,22 @@
+const btns = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".content");
+const about = document.querySelector(".about");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  // console.log(e.target)
+
+  // if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+
+    contents.forEach(function (article) {
+      article.classList.remove("active");
+    });
+
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  // }
+});
